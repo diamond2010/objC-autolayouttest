d4a79505426fd7d0f8b8a944d735252b96dd5da7
diamond2010 <herbst.thorsten@icloud.com>
2014-11-27 09:22:42 +0100

    Initial Commit

---
+++ autolayouttest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autolayouttest
+//
+//  Created by diamond on 27.11.14.
+//  Copyright (c) 2014 diamond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
